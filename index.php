93b4b4e250bdd03bcc1582e1068d778deff845d4
batuhan <batuhan_kanra_60@hotmail.com>
2023-02-24 01:01:36 +0300

    comit 2 tamamlandı ders biter

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$baglan=mysqli_connect("localhost","root","","baglantı")or die("mysql baglantisi başarısız");
+/* $baglan=mysqli_connect("localhost","root","","baglantı")or die("mysql baglantisi başarısız");
 mysqli_set_charset($baglan,"utf8");
 //$sorgu=mysqli_query($baglan,"insert into ayar values('','batuhan1','12345','batuhan1 kanra php koduyla ekleme')");
 //$sorgu=mysqli_query($baglan,"update ayar set icerik_ad='icerik_bolum2' where (id=3)" );
@@ -8,8 +8,13 @@ mysqli_set_charset($baglan,"utf8");
 //$sonuc=mysqli_affected_rows($baglan);
 
 $sorgu=mysqli_query($baglan,"select * from ayar");
-$satir=mysqli_fetch_row($sorgu);
-echo "adı soyadı:$satir[1] - icerik adı=$satir[3]";
+while($satir=mysqli_fetch_assoc($sorgu)){
+    echo "adı soyadı:$satir[kullanici_adi] - sifre=$satir[sifre]";
+}
 
+$toplam=mysqli_num_rows($sorgu);
+echo "toplam $toplam kayıt bulundu";
+*/
 
+$baglan=new mysqli("localhost","root","","baglantı");
 ?>
