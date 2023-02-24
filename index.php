fa88ac164e8d139fcf2d8b6e504b2c7d376029aa
batuhan <batuhan_kanra_60@hotmail.com>
2023-02-24 18:33:58 +0300

    eğitim tamamlandı mysqli query öğrenildi

--- index.php
+++ index.php
@@ -14,7 +14,63 @@ while($satir=mysqli_fetch_assoc($sorgu)){
 
 $toplam=mysqli_num_rows($sorgu);
 echo "toplam $toplam kayıt bulundu";
+
+$baglan=mysqli_connect("localhost","root","","baglantı")or die("mysql bağlantısı başarısız");
+mysqli_set_charset($baglan,"utf8");
+
+
+
+$sorgu=mysqli_query($baglan,"select * from ayar");
+/*while($satir=mysqli_fetch_assoc($sorgu)){
+    echo "kullanici adi: $satir[kullanici_adi]- sifresi: $satir[sifre] -aciklama : $satir[icerik_ad]";
+    echo "<br>";
+}
+while($satir=mysqli_fetch_object($sorgu)){
+    echo "kullanici adi: $satir->kullanici_adi sifresi $satir->sifre -aciklama: $satir->icerik_ad";
+    echo "<br> <br>";
+}
+$toplam=mysqli_num_rows($sorgu);
+echo "toplam $toplam kayıt bulundu <br>";
+$sonuc=mysqli_affected_rows($baglan);
+echo "$sonuc adet kayıt güncellendi";
+
+mysqli_free_result($sorgu);
+mysqli_close($baglan);
 */
 
-$baglan=new mysqli("localhost","root","","baglantı");
+// güvenli bir veri tabanı bağlantısı
+$baglan=new mysqli("localhost","root","","baglantı")or die("bağlantı hatası");
+$baglan->set_charset("utf8");
+
+/* güvenli bir şekilde veri tabanına veri ekleme
+$sorgu=$baglan->query("INSERT INTO ayar values('','batuhankan1','123456789','açıklama seti girildi')");
+*/
+
+// veri tabanındaki verileri çekme
+/*$sorgu=$baglan->query("SELECT * FROM ayar");
+
+while($satir=$sorgu->fetch_object()){
+    echo "Kullanıcı adı :$satir->kullanici_adi sifresi:$satir->sifre aciklama: $satir->icerik_ad <br> <br>";
+}
+$sorgu->free_result();
+*/
+
+//veri tabanına kayıt ekleme
+
+$sorgu=$baglan->prepare("INSERT INTO ayar (kullanici_adi,sifre,icerik_ad) VALUES(?,?,?)");
+
+$sorgu->bind_param("sss",$adsoyad,$sifre,$aciklama);
+$adsoyad="kullanici adi 1";
+$sifre="11111";
+$aciklama="nesne yönelimli açıklama1";
+$sorgu->execute();
+
+$adsoyad="kullanici adi 2";
+$sifre="22222";
+$aciklama="nesne yönelimli açıklama2";
+$sorgu->execute();
+
+$sorgu->close();
+
+$baglan->close();
 ?>
